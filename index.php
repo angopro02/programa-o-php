595afd7577446db580e1acbccccd5b56ce9ecf1e
angopro02 <112010770+angopro02@users.noreply.github.com>
2022-11-07 17:00:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <H3>CADASTRO CLIENTES</H3>
+    <hr>
+    <a href="cadastrar.php">Cadastrar Cliente </a><br/>
+    <a href="consultar.php">Consultar Cliente </a>
+</body>
+</html>
