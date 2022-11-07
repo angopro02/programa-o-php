c4b564b4fea1067b062abae152c1b1880c807dd6
angopro02 <112010770+angopro02@users.noreply.github.com>
2022-11-07 16:59:44 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de Cliente</h1>
+    <form action="gravar.php" method="post">
+    Nome: <br/>
+    <input type="text" name="nome" id=""><br/>
+    E-mail: <br/><br/>
+    <input type="text" name="email" id=""><br/>
+    Telefone:<br/><br/>
+    <input type="text" name="telefone" id=""><br/>
+    <br/><br/>
+    <input type="submit" value= "cadastrar">
+
+
+    </form>
+    
+</body>
+</html>
